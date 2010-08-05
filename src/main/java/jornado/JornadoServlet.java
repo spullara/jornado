58175380ea8c36cfe013be8c9e8bb2373f679ff4
John Beatty <john.beatty@gmail.com>
2010-08-05 11:15:13 +0800

    various cleanups

--- src/main/java/jornado/JornadoServlet.java
+++ src/main/java/jornado/JornadoServlet.java
@@ -21,6 +21,8 @@ import java.util.List;
  * TODO: I've gone to some pains to allow an app-specified Request object but didn't close the loop on this
  */
 public class JornadoServlet<R extends Request<U>, U extends WebUser> extends HttpServlet {
+  private static final long serialVersionUID = 6175864095769782000L;
+
   private final Router<R> router;
   private final UserService<U> userService;
   private final SecureCookieService secureCookieService;
